5ff853db6eb85fbf9c6f3aeb36222905b79f901a
gzp <z.p.gaal@gmail.com>
2016-09-02 17:44:29 +0200

    appveyor deploy test_1

--- appveyor/deploy.bat
+++ appveyor/deploy.bat
@@ -1,6 +1,3 @@
-set QTDIR=d:\Qt\5.4\mingw491_32
-set BUILDDIR=build-d2d-Desktop_Qt_5_4_2_MinGW_32bit-Release\release
-
 @echo off
 set PATH=%PATH%;%QTDIR%\bin
 
@@ -12,9 +9,9 @@ md deploy\d2d
 @echo deploying
 copy "%BUILDDIR%\d2d.exe" deploy\d2d\
 copy "LICENSE" deploy\d2d\
-copy "%QTDIR%\bin\libgcc_s_dw2-1.dll" deploy\d2d\
-copy "%QTDIR%\bin\libstdc++-6.dll" deploy\d2d\
-copy "%QTDIR%\bin\libwinpthread-1.dll" deploy\d2d\
+rem copy "%QTDIR%\bin\libgcc_s_dw2-1.dll" deploy\d2d\
+rem copy "%QTDIR%\bin\libstdc++-6.dll" deploy\d2d\
+rem copy "%QTDIR%\bin\libwinpthread-1.dll" deploy\d2d\
 
 windeployqt --compiler-runtime --dir deploy\d2d %BUILDDIR%\d2d.exe
 
@@ -22,4 +19,3 @@ windeployqt --compiler-runtime --dir deploy\d2d %BUILDDIR%\d2d.exe
 cd deploy
 zip -9 d2d.zip d2d\*
 cd ..
-
