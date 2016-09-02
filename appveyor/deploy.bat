e2e35573637208ff60c46872ab1e59428df3c63e
zpgaal <z.p.gaal.devel@gmail.com>
2016-09-02 18:52:19 +0200

    Update deploy.bat

--- appveyor/deploy.bat
+++ appveyor/deploy.bat
@@ -6,9 +6,9 @@ md deploy\%DEPLOY_NAME%
 @echo deploying
 copy "%BUILDDIR%\d2d.exe" deploy\%DEPLOY_NAME%\
 copy "LICENSE" deploy\%DEPLOY_NAME%\
-rem copy "%QTDIR%\bin\libgcc_s_dw2-1.dll" deploy\%DEPLOY_NAME%\
-rem copy "%QTDIR%\bin\libstdc++-6.dll" deploy\%DEPLOY_NAME%\
-rem copy "%QTDIR%\bin\libwinpthread-1.dll" deploy\%DEPLOY_NAME%\
+copy "%QTDIR%\bin\libgcc_s_dw2-1.dll" deploy\%DEPLOY_NAME%\
+copy "%QTDIR%\bin\libstdc++-6.dll" deploy\%DEPLOY_NAME%\
+copy "%QTDIR%\bin\libwinpthread-1.dll" deploy\%DEPLOY_NAME%\
 
 windeployqt --compiler-runtime --dir deploy\%DEPLOY_NAME% %BUILDDIR%\d2d.exe
 
