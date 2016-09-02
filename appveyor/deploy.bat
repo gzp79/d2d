b9a6b46b590d7c0151c247b3b4eadde69b147e77
zpgaal <z.p.gaal.devel@gmail.com>
2016-09-02 18:02:03 +0200

    Update deploy.bat

--- appveyor/deploy.bat
+++ appveyor/deploy.bat
@@ -1,5 +1,3 @@
-set DEPLOY_NAME = "d2d-%PLATFORM%-%APPVEYOR_BUILD_VERSION%"
-
 @echo setting up deploy dir
 rd /S /Q deploy
 md deploy
@@ -14,9 +12,7 @@ rem copy "%QTDIR%\bin\libwinpthread-1.dll" deploy\%DEPLOY_NAME%\
 
 windeployqt --compiler-runtime --dir deploy\%DEPLOY_NAME% %BUILDDIR%\d2d.exe
 
-dir deploy\
-dir deploy\%DEPLOY_NAME%
 @echo compressing
 cd deploy
-7z a %DEPLOY_ZIP%.7z %DEPLOY_NAME%
+7z a %DEPLOY_NAME%.7z %DEPLOY_NAME%
 cd ..
