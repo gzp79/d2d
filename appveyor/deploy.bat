75fd59a61b680e4f648822cc7fb9dd03308f52a4
zpgaal <z.p.gaal.devel@gmail.com>
2016-09-02 17:57:15 +0200

    Update deploy.bat

--- appveyor/deploy.bat
+++ appveyor/deploy.bat
@@ -1,20 +1,22 @@
+set DEPLOY_NAME = "d2d-%PLATFORM%-%APPVEYOR_BUILD_VERSION%"
+
 @echo setting up deploy dir
 rd /S /Q deploy
 md deploy
-md deploy\d2d
+md deploy\%DEPLOY_NAME%
 
 @echo deploying
-copy "%BUILDDIR%\d2d.exe" deploy\d2d\
-copy "LICENSE" deploy\d2d\
-rem copy "%QTDIR%\bin\libgcc_s_dw2-1.dll" deploy\d2d\
-rem copy "%QTDIR%\bin\libstdc++-6.dll" deploy\d2d\
-rem copy "%QTDIR%\bin\libwinpthread-1.dll" deploy\d2d\
+copy "%BUILDDIR%\d2d.exe" deploy\%DEPLOY_NAME%\
+copy "LICENSE" deploy\%DEPLOY_NAME%\
+rem copy "%QTDIR%\bin\libgcc_s_dw2-1.dll" deploy\%DEPLOY_NAME%\
+rem copy "%QTDIR%\bin\libstdc++-6.dll" deploy\%DEPLOY_NAME%\
+rem copy "%QTDIR%\bin\libwinpthread-1.dll" deploy\%DEPLOY_NAME%\
 
-windeployqt --compiler-runtime --dir deploy\d2d %BUILDDIR%\d2d.exe
+windeployqt --compiler-runtime --dir deploy\%DEPLOY_NAME% %BUILDDIR%\d2d.exe
 
 dir deploy\
-dir deploy\d2d
-rem @echo compressing
-rem cd deploy
-rem zip -9 d2d.zip d2d\*
-rem cd ..
+dir deploy\%DEPLOY_NAME%
+@echo compressing
+cd deploy
+7z a %DEPLOY_ZIP%.7z %DEPLOY_NAME%
+cd ..
